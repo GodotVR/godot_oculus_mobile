606e6a5d7c2b732ee1ffe22973cdf21a62d07dd2
Holger Dammertz <holger.dammertz@googlemail.com>
2019-10-01 18:14:17 +0200

    brief method documentation in header

--- src/config/ovr_utilities.h
+++ src/config/ovr_utilities.h
@@ -10,13 +10,16 @@
 extern "C" {
 #endif
 
+// registers the OvrUtilities class and functions to GDNative and should be called from godot_ovrmobile_nativescript_init
 void register_gdnative_utilities(void *p_handle);
 
 GDCALLINGCONV void *ovr_utilities_constructor(godot_object *p_instance, void *p_method_data);
 GDCALLINGCONV void ovr_utilities_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
 
+// uses the internal left and right view matrix to compute the IPD. Returns a float in GDScript
 GDCALLINGCONV godot_variant get_ipd(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args);
 
+// Expects a Color parameter in GDScript and sets the color multiplier for the default layer used by the VrAPI compositor
 GDCALLINGCONV godot_variant set_default_layer_color_scale(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args);
 
 
