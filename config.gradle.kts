948f07e516ad294531c92caef5f76688aac3cfab
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2020-06-03 17:11:52 -0700

    Update project dependencies

---
+++ config.gradle.kts
@@ -0,0 +1,7 @@
+val kotlinVersion by extra("1.3.72")
+val gradlePluginVersion by extra("4.0.0")
+
+val compileSdk by extra(29)
+val buildTools by extra("29.0.3")
+val minSdk by extra(18)
+val targetSdk by extra(29)
