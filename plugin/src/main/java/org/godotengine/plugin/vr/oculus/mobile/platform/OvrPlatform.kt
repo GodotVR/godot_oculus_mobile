1aa2bfa8182d4bd4e2d85cb937c0a70bcfe0e909
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2021-06-21 23:02:14 -0700

    Linking the Oculus platform SDK shared library.

---
+++ plugin/src/main/java/org/godotengine/plugin/vr/oculus/mobile/platform/OvrPlatform.kt
@@ -0,0 +1,11 @@
+@file:JvmName("OvrPlatform")
+
+package org.godotengine.plugin.vr.oculus.mobile.platform
+
+import org.godotengine.plugin.vr.oculus.mobile.OvrMobilePlugin
+
+/**
+ *
+ */
+class OvrPlatform {
+}
